7c0418b76dba7740d650b4597ed28792341f2246
Nicolas <nicojs2011@gmail.com>
2024-03-14 12:13:35 -0400

    cors añadido

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express =  require('express');
+const cors = require('cors')
 const app = express();
 require('dotenv').config()
 
 const port = process.env.API_PORT || 3000;
+
+app.use(cors()) // Todo el mundo
 
 //middlewares
 app.use(express.json())
