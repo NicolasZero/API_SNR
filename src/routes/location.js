466d17f7fa985184185fb7f33ac05c9286a3f0cd
Nicolas Zapata <nicojs2011@gmail.com>
2024-04-02 14:20:26 -0400

    ruta location municipality y parish cambiadas

--- src/routes/location.js
+++ src/routes/location.js
@@ -8,13 +8,11 @@ router.get('/state/name/:val',stateBy('state'))
 
 router.get('/municipality',municipalitiesAll)
 router.get('/municipality/id/:val',municipalityBy('id'))
-router.get('/municipality/name/:val',municipalityBy('municipality'))
 router.get('/municipality/state/:val',municipalityBy('state_id'))
 // router.get('/',state)
 
 router.get('/parish',parishAll)
 router.get('/parish/id/:val',parishBy('id'))
-router.get('/parish/name/:val',parishBy('parish'))
 router.get('/parish/municipality/:val',parishBy('municipality_id'))
 
 
