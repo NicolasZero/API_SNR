c56f0c030bdcb3cf8b515a0e488fa7801de37a60
Nicolas Zapata <nicojs2011@gmail.com>
2024-04-17 11:56:42 -0400

    status failed cambiado a ok en registrar usuarios

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -76,7 +76,7 @@ const createItem = async (req, res) => {
             let resp = await query('INSERT INTO persons (identity_card, is_foreign, first_name, other_names, first_last_name, other_last_names, email, phone, gender_id) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING id', [identity_card, is_foreign, first_name, other_names, first_last_name, other_last_names, email, phone, gender_id])
             const person_id = resp.rows[0].id
             resp = await query('INSERT INTO auth.users (username, password, person_id, role_id, department_id, is_active) VALUES ($1,$2,$3,$4,$5,$6) RETURNING id', [username, hash, person_id, role_id, department_id, true])
-            res.json({ status: "FAILED", data: resp.rows })
+            res.json({ status: "OK", data: resp.rows })
         }
     } catch (error) {
         httpError(res, error)
